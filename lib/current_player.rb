908dfdcb3388f4d9d0e894165b76d51f3e27fea6
Julie <juliejhudspeth@gmail.com>
2018-05-18 16:58:41 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  if token = "X" or token = "O"
+  counter +=1
+  end
+end
+
+def current_player(board)
+  if counter % 2
+    token == "X"
+  else
+    token == "O"
+  end
+end
